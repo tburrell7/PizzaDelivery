e447b1d5fc6dab5996eef0d3a3a2dbe375b36d29
tburrell7 <tburrell7@gmail.com>
2022-05-31 20:24:40 -0400

    Unit Tests

--- internal/deliver/pizzaDelivery.go
+++ internal/deliver/pizzaDelivery.go
@@ -6,12 +6,13 @@ import (
 	"pizzaDelivery/internal/schemes"
 )
 
-var outOfBoundsError = errors.New("out of bounds error")
-var invalidInputError = errors.New("invalid input error")
+//Types of errors
+var OutOfBoundsError = errors.New("out of bounds error")
+var InvalidInputError = errors.New("invalid input error")
 
+//Returns number of houses visited and any errors
 func PizzaDelivery(input string) (uint64, error) {
-	//iterate through input, validating along the way. Store position and times delivered in hashmap
-	//store result in hash map
+	//Instantiate variables
 	var err error
 	var res uint64
 	var x uint64 = 2^63
@@ -19,7 +20,9 @@ func PizzaDelivery(input string) (uint64, error) {
 	m := make(map[schemes.Address]uint64)
 	m[schemes.Address{X: x, Y: y}] += 1
 
+	//Iterate through input
 	for i, c := range input {
+		//Validate each character
 		switch string(c) {
 		case "^":
 			y += 1
@@ -29,13 +32,21 @@ func PizzaDelivery(input string) (uint64, error) {
 			x += 1
 		case "v", "V":
 			y -= 1
+		//If invalid input, return InvalidInputError
 		default:
 			res = uint64(len(m))
-			err = fmt.Errorf("%w at index %d", invalidInputError, i)
+			err = fmt.Errorf("%w at index %d", InvalidInputError, i)
 			return res, err
 		}
+		//If out of bounds, return OutOfBoundsError
+		if x == 0 || y == 0 || x == 2^64-1 || y == 2^64-1 {
+			err = fmt.Errorf("%w at index %d", OutOfBoundsError, i)
+			return res, err
+		}
+		//Increment number of deliveries to an address by 1 and store in the map
 		m[schemes.Address{X: x, Y: y}] += 1
 	}
+	//Only visited houses will exist in the map
 	res = uint64(len(m))
 	return res, err
 }
--- internal/deliver/pizzaDelivery_test.go
+++ internal/deliver/pizzaDelivery_test.go
@@ -1,2 +1,42 @@
 package deliver_test
 
+import (
+	"pizzaDelivery/internal/deliver"
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPizzaDelivery(t *testing.T) {
+	t.Run("Correct Output", func(t *testing.T) {
+		var expectedOutput uint64 = 2
+		res, err := deliver.PizzaDelivery("^v^v")
+		require.NoError(t, err)
+		require.Equal(t, res, expectedOutput)
+	})
+	t.Run("Correct Output", func(t *testing.T) {
+		var expectedOutput uint64 = 4
+		res, err := deliver.PizzaDelivery("^>V<")
+		require.NoError(t, err)
+		require.Equal(t, res, expectedOutput)
+	})
+	t.Run("Correct Output", func(t *testing.T) {
+		var expectedOutput uint64 = 2
+		res, err := deliver.PizzaDelivery("^")
+		require.NoError(t, err)
+		require.Equal(t, res, expectedOutput)
+	})
+	t.Run("Empty Input", func(t *testing.T) {
+		var expectedOutput uint64 = 1
+		res, err := deliver.PizzaDelivery("")
+		require.NoError(t, err)
+		require.Equal(t, res, expectedOutput)
+	})
+	t.Run("Invalid Input", func(t *testing.T) {
+		_, err := deliver.PizzaDelivery("abcdefg")
+		require.Error(t, err)
+	})
+	t.Run("Invalid Input", func(t *testing.T) {
+		_, err := deliver.PizzaDelivery("^<>b")
+		require.Error(t, err)
+	})
+}
